8031c70556bb2b3a72fc89dbc55d432660bb0b02
dung1202 <70959545+dung1202@users.noreply.github.com>
2021-09-28 19:25:20 +0700

    Delete Truncate a String.js

--- Truncate a String.js
+++
@@ -1,8 +0,0 @@
-function truncateString(str, num) {
-  let a = str.slice(0,num)
-  if (a.length < str.length) a = a + '...'
- a
-  return a;
-}
-
-console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8))
